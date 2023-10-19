64703c1967b8de888cae383ea062c6cb7befa0d6
PanasheT <ptafuma@proton.me>
2023-10-19 18:54:15 +0200

    feat: add path constants

---
+++ src/constants/path.constants.ts
@@ -0,0 +1,9 @@
+export abstract class PathConstants {
+  public static readonly BASE = process.cwd();
+
+  public static readonly BUILD = PathConstants.BASE.concat("/build");
+
+  public static readonly BUILD_IMAGES = PathConstants.BUILD.concat("/images");
+
+  public static readonly BUILD_JSON = PathConstants.BUILD.concat("/json");
+}
