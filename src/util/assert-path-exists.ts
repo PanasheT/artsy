f0aae4e822cb9a2450638aee82be6beaaea4cd3f
PanasheT <ptafuma@proton.me>
2023-10-19 19:47:37 +0200

    feat: add path exists assertion func

---
+++ src/util/assert-path-exists.ts
@@ -0,0 +1,7 @@
+import { existsSync } from "fs";
+
+export default function assertPathExists(path: string): void {
+  if (!existsSync(path)) {
+    throw new Error(`Failed to locate: ${path}`);
+  }
+}
--- src/util/index.ts
+++ src/util/index.ts
@@ -2,3 +2,4 @@ export * from "./make-output-dir";
 export * from "./mkdir";
 export * from "./rmdir";
 export * from "./logger";
+export * from "./assert-path-exists";
