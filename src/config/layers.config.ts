1f16e43e4dcf6e2c89c56e9d97e647f2c904f2fe
PanasheT <ptafuma@proton.me>
2023-10-19 18:34:25 +0200

    feat: add layers config

---
+++ src/config/layers.config.ts
@@ -0,0 +1,19 @@
+type TLayer = {
+  name: string;
+};
+
+export type TLayerConfig = Readonly<{
+  collectionSize: number;
+  layersOrder: Readonly<Array<TLayer>>;
+}>;
+
+export const LAYERS_CONFIG: TLayerConfig = {
+  collectionSize: 100,
+  layersOrder: [
+    { name: "SKIN" },
+    { name: "NOSE" },
+    { name: "MOUTH" },
+    { name: "EYES" },
+    { name: "HEAD" },
+  ] as const,
+} as const;
