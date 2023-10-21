27f9dfb14a11e050af9f5b2e5580b99adfbf142b
PanasheT <112612442+PanasheT@users.noreply.github.com>
2023-10-21 17:28:31 +0200

    feat: add rarity delimiter

--- src/config/collection.config.ts
+++ src/config/collection.config.ts
@@ -2,10 +2,12 @@ type TCollectionConfig = Readonly<{
   name: string;
   author: string;
   description: string;
+  rarityDelimiter: '#' | '%' | '^';
 }>;
 
 export const COLLECTION_CONFIG: TCollectionConfig = {
   name: 'ProjectX',
   author: 'Panashe Innocent Tafuma',
   description: 'I have nothing cool to write...',
+  rarityDelimiter: '#',
 } as const;
